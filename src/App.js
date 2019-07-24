45fb204ad26b9a0c4a96c7c5a3c0c3ea4a9cd3d0
Lance Russell <lancebryanrussell@gmail.com>
2019-07-24 07:10:36 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import lander from './Images/SpaceLander/pixil-frame-0.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    lander: {
+      x: 10,
+      y: 10,
+      dy: 1,
+      radius: 20,
+    }
+  }
+
+  componentWillMount(){
+    const img = new Image(50, 50);  
+    img.src = lander;
+    img.onLoad = this.setState({img});
+  }
+
+  componentDidMount(){
+    this.update(); 
+  }
+
+  update = () => {
+    let lander = this.state.lander;
+
+    if(lander.y + lander.dy === (this.refs.canvas.height - 45)){
+      lander.dy = 0;
+    } else {
+      lander.y += lander.dy;
+    }
+    
+
+    this.setState({lander})
+    requestAnimationFrame(this.update);
+    this.draw();
+  }
+  
+  draw = () => {
+    let x = this.state.lander.x
+    let y = this.state.lander.y
+
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext("2d");
+    const img = this.state.img
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, x, y, 50, 50);
+  }
+
+  render(){
+    return (
+      <main className='App'>
+        <canvas ref="canvas" className="canvas">
+          <img id="image" ref="image" src={lander} className="lander" alt="lander" />
+        </canvas>
+      </main>
+    );
+  }
 }
 
 export default App;
